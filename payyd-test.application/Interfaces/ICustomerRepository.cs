4b87b9bbea12a1bca8409a65308a76ea3c0de1df
Jijo Jair (RCG) <JijoNRCG@abem.org>
2024-11-28 08:51:26 -0500

    Added Service layer

---
+++ payyd-test.application/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using payyd_test.domain.Common;
+
+namespace payyd_test.application.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        public Task<ResultOrError<string, string>> CreateCustomerOnPG();
+        public Task<ResultOrError<string, string>> GetAllCustomers();
+        public Task<ResultOrError<string, string>> GetCustomerById();
+
+    }
+}
---
+++ payyd-test.domain/Common/ResultOrError.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace payyd_test.domain.Common
+{
+    public class ResultOrError<TResult, TError>
+    {
+       
+            public ResultOrError(TResult result)
+            {
+                this.Result = result;
+                this.IsError = false;
+            }
+
+            public ResultOrError(TError error)
+            {
+                this.Error = error;
+                this.IsError = true;
+            }
+            public TResult Result { get; }
+
+            public TError Error { get; }
+
+            public bool IsError { get; }
+
+            public void Match(Action<TResult> onSuccess, Action<TError> onError)
+            {
+                if (this.IsError)
+                {
+                    onError(this.Error);
+                }
+                else
+                {
+                    onSuccess(this.Result);
+                }
+            }
+
+            public T Match<T>(Func<TResult, T> resultFunc, Func<TError, T> errorFunc) => this.IsError ?
+                                                                                errorFunc(this.Error) : resultFunc(this.Result);
+
+    }
+}
